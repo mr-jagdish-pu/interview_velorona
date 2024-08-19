b6637cbf13378ad1db80af219b5d15c528dcbb7b
jagdish bhai <jagdissghimire@gmail.com>
2024-08-19 19:39:39 +0545

    Final Commit

--- lib/feat/movie_display/pages/movie_component.dart
+++ lib/feat/movie_display/pages/movie_component.dart
@@ -14,61 +14,62 @@ class MovieComponent extends StatelessWidget {
         final url = Uri.parse(movie.website);
         await canLaunchUrl(url)
             ? launchUrl(url, mode: LaunchMode.externalApplication)
-            : ScaffoldMessenger.of(context)
-                .showSnackBar(SnackBar(content: Text('Cannot Launch Url')));
+            : ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Cannot Launch Url')));
       },
       child: Card(
         elevation: 5,
         surfaceTintColor: Colors.grey,
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    fit: BoxFit.cover,
-                    movie.poster,
-                    height: 200,
-                    width: 500,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  fit: BoxFit.cover,
+                  movie.poster,
+                  height: 200,
+                  width: 500,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    movie.title,
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(fontSize: 18),
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      movie.title,
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    Text(
-                      "On Year: " + movie.year.toString(),
-                      style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                            fontWeight: FontWeight.w700,
-                          ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Text('About'),
-                Wrap(
-                  alignment: WrapAlignment.start,
-                  crossAxisAlignment: WrapCrossAlignment.start,
-                  spacing: 20,
-                  children: movie.genre
-                      .map((e) => Chip(
-                            label: Text(e),
-                          ))
-                      .toList(),
-                )
-              ],
-            ),
+                  Text(
+                    "On Year: " + movie.year.toString(),
+                    style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                          fontWeight: FontWeight.w700,
+                        ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text('About'),
+              Wrap(
+                alignment: WrapAlignment.start,
+                crossAxisAlignment: WrapCrossAlignment.start,
+                spacing: 20,
+                children: movie.genre
+                    .map((e) => Chip(
+                          label: Text(e),
+                        ))
+                    .toList(),
+              )
+            ],
           ),
         ),
       ),
